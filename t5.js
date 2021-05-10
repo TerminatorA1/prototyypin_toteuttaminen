a60ce1a7ca331b8961c4326bb8cede966c07f90d
TerminatorA1 <62852208+TerminatorA1@users.noreply.github.com>
2021-05-10 13:15:31 +0300

    Add files via upload
    T5

---
+++ t5.js
@@ -0,0 +1,15 @@
+function testaa() {
+  var num1 = document.getElementById("k1").value;
+
+
+  console.log (num1);
+
+  if (num1 == "uk" ){
+      return alert("Hello world!");
+    } else if (num1 == "sp"){
+      return alert ("Hola mondo!");
+    } else {
+      return alert ("Hej jorden!")
+    }
+
+  }
